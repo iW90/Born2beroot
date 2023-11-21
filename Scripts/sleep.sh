b783fd49a2a0ee71677e8e2506ff385fa0d1811b
InGrid <93016084+iW90@users.noreply.github.com>
2023-11-21 19:49:35 -0300

    fix: change typo

--- Scripts/sleep.sh
+++ Scripts/sleep.sh
@@ -2,7 +2,7 @@
 
 # Get minutes and seconds
 bmin=$(uptime -s | cut -d ":" -f 2)
-msec=$(uptime -s | cut -d ":" -f 3)
+bsec=$(uptime -s | cut -d ":" -f 3)
 
 # Calculate number of seconds between the nearest 10th minute of the hour and boot time:
 delay=$(bc <<< $bmin%10*60+$bsec)
